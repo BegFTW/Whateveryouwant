a5af1e6d80a3553e74fe8d54fb434646f41b27e5
BegFTW <151565525+BegFTW@users.noreply.github.com>
2024-11-26 14:36:05 -0500

    Add files via upload

---
+++ Lab_5/lab5app/Program.cs
@@ -0,0 +1,35 @@
+using Lab_5.Components;
+using Lab_5.Services;
+
+namespace Lab_5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddScoped<ILibraryService, LibraryService>();
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
